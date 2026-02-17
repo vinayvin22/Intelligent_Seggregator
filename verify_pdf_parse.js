05ecafa1977cead5571da0d182f8904dd19875a4
vinay <vinaypatl2222@gmail.com>
2026-02-18 01:01:02 +0530

    Cleanup temporary test scripts

--- verify_pdf_parse.js
+++
@@ -1,7 +0,0 @@
-const pdfParse = require('pdf-parse');
-console.log('Type of pdf-parse:', typeof pdfParse);
-if (typeof pdfParse === 'function') {
-    console.log('SUCCESS: pdf-parse is a function');
-} else {
-    console.log('FAIL: pdf-parse is ' + typeof pdfParse);
-}
